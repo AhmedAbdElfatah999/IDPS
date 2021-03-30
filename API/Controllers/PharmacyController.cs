7f821989c6d311791a7f99fcf797100cb68be781
AhmedAbdElfatah999 <asd701622@gmail.com>
2021-03-30 15:09:12 +0200

    Pharmacy And Specialization Controllers Methods

--- API/Controllers/PharmacyController.cs
+++ API/Controllers/PharmacyController.cs
@@ -35,5 +35,78 @@ namespace API.Controllers
         {
            return await _PharmacyRepo.GetByIdAsync(id);
         }
+   //------------------------------------------------------
+   //Create New pharmacy
+    [HttpGet]
+    public IActionResult Create()
+        {
+            return Ok();
+        }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Phone,PictureUrl,NumberOfBranches")] Pharmacy pharmacy)
+        {
+             var file = Request.Form.Files[0];
+            if ((file != null && file.Length > 0))
+                {
+                  var fileName = Path.GetFileName(file.FileName);
+                  var path = Path.Combine("wwwroot/images/pharmacies", fileName);
+                  var fileStream = new FileStream(path, FileMode.Create);  
+                  file.CopyTo(fileStream);  
+                  pharmacy.PictureUrl = fileName;
+
+                }
+            if (ModelState.IsValid)
+            {
+                await _PharmacyRepo.Add(pharmacy);  
+            }else
+            {
+               return await BadRequest(new ApiResponse(400));
+            }
+            
+        }
+    //To delete pharmacy    
+    [HttpPost]
+    public async Task<IActionResult> Delete(pharmacy pharmacy)
+    {
+       await _PharmacyRepo.Delete(pharmacy);
+    }  
+
+    //to update pharmacy
+    [HttpGet]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult<Pharmacy>> Edit(int? id)
+    {
+            if (id == null)
+            {
+                return await BadRequest(new ApiResponse(400));
+            }
+
+            var pharmacy= await _PharmacyRepo.GetByIdAsync(id);
+            if (pharmacy == null)
+            {
+                return await BadRequest(new ApiResponse(400));
+            }
+            return pharmacy;
+    }
+   [HttpPost]
+   public async Task<IActionResult> Edit([Bind("Id,Name,Phone,PictureUrl,NumberOfBranches")] Pharmacy pharmacy)
+   {
+      if (Request.Form.Files.Any())
+         {
+            var file = Request.Form.Files[0];
+            var fileName = Path.GetFileName(file.FileName);
+            var path = Path.Combine("wwwroot/images/pharmacies", fileName);
+            var fileStream = new FileStream(path, FileMode.Create);          
+            file.CopyTo(fileStream);  
+            Pharmacy.PictureUrl = fileName;             
+            _PharmacyRepo.Update(pharmacy);
+            return await Ok();                    
+
+        }else{
+            _PharmacyRepo.Update(pharmacy);
+            return await Ok();
+            }
+   }           
     }
 }
--- API/Controllers/SpecializationController.cs
+++ API/Controllers/SpecializationController.cs
@@ -35,5 +35,57 @@ namespace API.Controllers
         {
            return await _SpecializationRepo.GetByIdAsync(id);
         }
+ //------------------------------------------------------
+   //Create New Specialization
+    [HttpGet]
+    public IActionResult Create()
+        {
+            return Ok();
+        }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name")] Specialization specialization)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _SpecializationRepo.Add(specialization);  
+            }else
+            {
+               return await BadRequest(new ApiResponse(400));
+            }
+            
+        }
+    //To delete Specialization    
+    [HttpPost]
+    public async Task<IActionResult> Delete(Specialization specialization)
+    {
+       await _SpecializationRepo.Delete(specialization);
+    }  
+
+    //to update Specialization
+    [HttpGet]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult<Specialization>> Edit(int? id)
+    {
+            if (id == null)
+            {
+                return await BadRequest(new ApiResponse(400));
+            }
+
+            var specialization= await _SpecializationRepo.GetByIdAsync(id);
+            if (specialization == null)
+            {
+                return await BadRequest(new ApiResponse(400));
+            }
+            return specialization;
+    }
+   [HttpPost]
+   public async Task<IActionResult> Edit([Bind("Id,Name")] Specialization Specialization)
+   {
+                   
+         _SpecializationRepo.Update(Specialization);
+            return await Ok();                    
+   }                   
     }
 }
